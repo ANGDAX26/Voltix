44af908ef018ff37c9c49e08d9e782aa678f1aac
kiritoxdxd <adriansolis4989@gmail.com>
2026-07-27 11:56:17 -0600

    a

--- PHP/conexion.php
+++ PHP/conexion.php
@@ -1,7 +1,7 @@
 <?php
 // Conexión a la base de datos "voltix"
 $servidor = "localhost";
-$puerto = "añada su puerto";
+$puerto = "5432"; 
 $usuario_db = "root";
 $password_db = "";
 $base_datos = "voltix";
